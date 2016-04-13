e09ea935e1498e071067d03405f60d6318ce8dcd
Alessandro Santamaria <a.reichgruber@gmail.com>
2016-04-13 02:26:19 +0000

    basic setup

---
+++ www/js/controllers.js
@@ -0,0 +1,38 @@
+angular.module('app.controllers', [])
+        
+.controller('resultsCtrl', function($scope) {
+
+})
+   
+.controller('criteriasCtrl', function($scope) {
+
+})
+   
+.controller('addCriteriaCtrl', function($scope) {
+
+})
+   
+.controller('editCriteriaCtrl', function($scope) {
+
+})
+   
+.controller('loginCtrl', function($scope) {
+
+})
+   
+.controller('signupCtrl', function($scope) {
+
+})
+   
+.controller('settingsCtrl', function($scope) {
+
+})
+   
+.controller('listingCtrl', function($scope) {
+
+})
+   
+.controller('premiumCtrl', function($scope) {
+
+})
+ 
---
+++ www/js/routes.js
@@ -0,0 +1,93 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+  
+
+      .state('tabsController', {
+    url: '/page1',
+    templateUrl: 'templates/tabsController.html',
+    abstract:true
+  })
+
+  .state('tabsController.results', {
+    url: '/results',
+    views: {
+      'tab1': {
+        templateUrl: 'templates/results.html',
+        controller: 'resultsCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.criterias', {
+    url: '/criterias',
+    views: {
+      'tab3': {
+        templateUrl: 'templates/criterias.html',
+        controller: 'criteriasCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.addCriteria', {
+    url: '/add',
+    views: {
+      'tab4': {
+        templateUrl: 'templates/addCriteria.html',
+        controller: 'addCriteriaCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.editCriteria', {
+    url: '/edit',
+    views: {
+      'tab3': {
+        templateUrl: 'templates/editCriteria.html',
+        controller: 'editCriteriaCtrl'
+      }
+    }
+  })
+
+  .state('login', {
+    url: '/login',
+    templateUrl: 'templates/login.html',
+    controller: 'loginCtrl'
+  })
+
+  .state('signup', {
+    url: '/signup',
+    templateUrl: 'templates/signup.html',
+    controller: 'signupCtrl'
+  })
+
+  .state('settings', {
+    url: '/settings',
+    templateUrl: 'templates/settings.html',
+    controller: 'settingsCtrl'
+  })
+
+  .state('listing', {
+    url: '/listing',
+    templateUrl: 'templates/listing.html',
+    controller: 'listingCtrl'
+  })
+
+  .state('premium', {
+    url: '/premium',
+    templateUrl: 'templates/premium.html',
+    controller: 'premiumCtrl'
+  })
+
+$urlRouterProvider.otherwise('/page1/results')
+
+  
+
+});
