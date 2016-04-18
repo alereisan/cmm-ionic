06aa91bc34ade76353274e01f82a4f054cdad2a4
Alessandro Santamaria <a.reichgruber@gmail.com>
2016-04-18 18:33:14 +0000

    redirectUri localhost

--- www/js/auth.js
+++ www/js/auth.js
@@ -13,7 +13,7 @@ angular.module('app.auth', [])
 
   if (ionic.Platform.isIOS() || ionic.Platform.isAndroid()) {
     $authProvider.cordova = true;
-    commonConfig.redirectUri = 'http://52.58.72.73:8080/auth/callback/';
+    commonConfig.redirectUri = 'http://localhost/';
   };
 
   $authProvider.facebook(angular.extend({}, commonConfig, {
